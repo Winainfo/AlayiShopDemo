0372be5722d65ff4fc7eace52e080a49f3ae14b5
吴金林 <kolin@wujinlindeMacBook-Pro.local>
2015-06-30 13:59:12 +0800

    2015-6-30 14:00 创建AlayiShopDemo项目 ----吴金林

---
+++ AlayiShopDemo/AlayiShopDemo/Classes/View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlayiShopDemo
+//
+//  Created by 吴金林 on 15/6/30.
+//  Copyright (c) 2015年 kolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
