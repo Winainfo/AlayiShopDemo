5793a4b10991ab578207f16494bce6ffd7d0a840
吴金林 <kolin@wujinlindeMacBook-Pro.local>
2015-06-30 14:45:25 +0800

    2015-6-30 14:35 创建临时文件 －－－吴金林

---
+++ AlayiShopDemo/AlayiShopDemo/Classes/Main/Common/CommonViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommonViewController.h
+//  AlayiShopDemo
+//
+//  Created by 吴金林 on 15/6/30.
+//  Copyright (c) 2015年 kolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonViewController : UIViewController
+
+@end
---
+++ AlayiShopDemo/AlayiShopDemo/Classes/Main/Personal/PersonalViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonalViewController.h
+//  AlayiShopDemo
+//
+//  Created by 吴金林 on 15/6/30.
+//  Copyright (c) 2015年 kolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonalViewController : UIViewController
+
+@end
