2f4c879a498d1bfcfd9ce3e299fdb82f6a0e719e
ibokan <ibokan@yanfayi16.local>
2015-07-01 21:58:53 +0800

    2015-7-1 9:58 九宫格的制作未完成----吴文成

--- AlayiShopDemo/AlayiShopDemo/Classes/Main/Category/Controller/SequenceCollectionViewCell.m
+++ AlayiShopDemo/AlayiShopDemo/Classes/Main/Category/Controller/SequenceCollectionViewCell.m
@@ -3,7 +3,34 @@
 @implementation SequenceCollectionViewCell
 
 - (void)awakeFromNib {
-    // Initialization code
+    //关闭交互
+    self.nameText.userInteractionEnabled = NO;
+    //cell单元格倒角
+    self.viewForBaselineLayout.layer.cornerRadius = 6.0;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        // 初始化时加载collectionCell.xib文件
+        NSArray *arrayOfViews = [[NSBundle mainBundle] loadNibNamed:@"SequenceCollectionViewCell"/*标示符*/ owner:self options:nil];
+        
+        // 如果路径不存在，return nil
+        if (arrayOfViews.count < 1)
+        {
+            return nil;
+        }
+        // 如果xib中view不属于UICollectionViewCell类，return nil
+        if (![[arrayOfViews objectAtIndex:0] isKindOfClass:[UICollectionViewCell class]])
+        {
+            return nil;
+        }
+        // 加载nib
+        self = [arrayOfViews objectAtIndex:0];
+    }
+    return self;
 }
 
 @end
