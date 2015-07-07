7cbc653cfc41582f701c45d9eb91131ed4666676
ibokan <ibokan@yanfayi16.local>
2015-07-07 20:41:38 +0800

    2015-7-7付款界面搭建完成，未适配----吴文成

---
+++ AlayiShopDemo/AlayiShopDemo/Classes/Main/Cart/InventoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  InventoryViewController.h
+//  AlayiShopDemo
+//
+//  Created by ibokan on 15/7/7.
+//  Copyright (c) 2015年 kolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InventoryViewController : UIViewController
+
+@end
