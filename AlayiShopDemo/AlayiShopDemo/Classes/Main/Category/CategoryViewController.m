0e15c59379c09728b0a4e177c2877d4a3c55e81c
吴金林 <kolin@wujinlindeMacBook-Pro.local>
2015-07-13 00:28:28 +0800

    2015-7-12 完成分类结果页---吴金林

--- AlayiShopDemo/AlayiShopDemo/Classes/Main/Category/CategoryViewController.m
+++ AlayiShopDemo/AlayiShopDemo/Classes/Main/Category/CategoryViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "CategoryViewController.h"
-
-@interface CategoryViewController ()
+#import "Category_Cell.h"
+//获得当前屏幕宽高点数（非像素）
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
+#define kScreenWidth  [UIScreen mainScreen].bounds.size.width
+@interface CategoryViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
+@property (weak, nonatomic) IBOutlet UICollectionView *CollectionView;
 
 @end
 
@@ -16,22 +20,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    //注册
+    [self.CollectionView registerClass:[Category_Cell class] forCellWithReuseIdentifier:@"Category_Cell"];
+}
+#pragma mark 实现代理方法
+//每个section的item个数
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 12;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    Category_Cell *cell = (Category_Cell *)[collectionView dequeueReusableCellWithReuseIdentifier:@"Category_Cell" forIndexPath:indexPath];
+    return cell;
+}
+#pragma mark 实现UICollectionViewDelegateFlowLayout代理
+/**
+ *  定义每个UICollectionView的间距
+ *
+ *  @param collectionView       <#collectionView description#>
+ *  @param collectionViewLayout <#collectionViewLayout description#>
+ *  @param section              <#section description#>
+ *
+ *  @return <#return value description#>
+ */
+-(UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
+{
+    if (kScreenWidth==320||kScreenWidth==375) {
+        return UIEdgeInsetsMake(5, 5, 5, 5);
+    }
+    return UIEdgeInsetsMake(0,0,0,0);
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (kScreenWidth==320||kScreenWidth==375) {
+        return CGSizeMake(150, 140);
+    }
+    return CGSizeMake(130, 140);
 }
-*/
-
 @end
