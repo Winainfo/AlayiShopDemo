7849338558517c7988a1c35f10bee4bb739330b3
吴金林 <kolin@wujinlindeMacBook-Pro.local>
2015-07-13 11:35:21 +0800

    2015-7-13 增加分类id

--- AlayiShopDemo/AlayiShopDemo/Classes/Main/Category/Controller/FinallyViewController.h
+++ AlayiShopDemo/AlayiShopDemo/Classes/Main/Category/Controller/FinallyViewController.h
@@ -11,7 +11,6 @@
 #import "AppDelegate.h"
 
 
-
 @interface FinallyViewController : UIViewController<UICollectionViewDataSource,UICollectionViewDelegate>
 @property (strong, nonatomic) IBOutlet UICollectionView *cellView;
 
